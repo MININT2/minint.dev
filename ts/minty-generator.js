af3f89c2f01e8af1f7910213d1d0f3061b37bb51
MININT2 <minint.dsk@gmail.com>
2021-12-27 14:00:40 -0600

    added more mintys

--- ts/minty-generator.js
+++ ts/minty-generator.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const escape_unicode_1 = require("../js/escape-unicode");
+import escapeUnicode from '../js/escape-unicode';
 let eyes = ["● ●", "✧ ✧", "ㅇ ㅇ", "◕ ◕", "◓ ◓", "> <", ". .", "｡ ｡", "- -", "✪ ✪", "◔ ◔", "⦿ ⦿", "⦾ ⦾"];
 let mouths = ["ㅅ", "×", "_", "ꞈ", "∇", "｡", "ᆽ", "ꞈ", "︿"];
 let bodies = ["   ", "( )", "/ᐠ ᐟ\\"];
@@ -34,7 +32,7 @@ let random = Math.floor(Math.random() * (hardcoded.length / 2));
 let titleElement = document.getElementById('title');
 const dna = hardcoded[0 + (random * 2)];
 if (titleElement)
-    document.title = (0, escape_unicode_1.default)(dna);
+    document.title = escapeUnicode(dna);
 //minty-generator is supposed to pull strings to make a cat
 //and set the page title to a newly generated cat periodically
 //yes, i do want cute cats to adorn your browser bar
