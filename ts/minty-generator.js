e9615ef1b32a156f1233d8a496cb3684a267e3fd
MININT2 <minint.dsk@gmail.com>
2021-12-27 18:16:12 -0600

    new bug, now escaped unicode strings are displayed as literals now

--- ts/minty-generator.js
+++ ts/minty-generator.js
@@ -1,41 +1,58 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const escape_unicode_1 = require("../js/escape-unicode");
-let eyes = ["● ●", "✧ ✧", "ㅇ ㅇ", "◕ ◕", "◓ ◓", "> <", ". .", "｡ ｡", "- -", "✪ ✪", "◔ ◔", "⦿ ⦿", "⦾ ⦾"];
-let mouths = ["ㅅ", "×", "_", "ꞈ", "∇", "｡", "ᆽ", "ꞈ", "︿"];
-let bodies = ["   ", "( )", "/ᐠ ᐟ\\"];
-let emote = ["  ", "❀", "ฅ", "*", "==", "✿"];
-let filler = [" ", "ᵐᵉᵒʷˎˊ˗", "🆄🆆🆄"];
-let hardcoded = [
-    "(●ㅅ● )", "n",
-    "( ✧×✧)", "n",
-    "(ㅇㅅㅇ❀)", "n",
-    "(ฅ^･ㅅ･^)", "n",
-    "(◕ㅅ◕ )", "n",
-    "( ◓-◓)", "n",
-    "(* >ㅅ<)", "n",
-    "(   ∵   )", "n",
-    "=^._.^= ∫", "n",
-    "(=´∇｀=)", "n",
-    "/ᐠ｡ꞈ｡ᐟ\\", "n",
-    "✧/ᐠ-ꞈ-ᐟ\\", "n",
-    "/ᐠ.ﮧ.ᐟ\\", "n",
-    "/ᐠ. ｡.ᐟ\\ᵐᵉᵒʷˎˊ˗", "n",
-    "/ᐠ-ᆽ-ᐟ\\", "n",
-    "/ᐠ ̥  ̮  ̥ ᐟ\\ฅ", "n",
-    "(₌◕ᆽ◕₌)", "n",
-    "(₌●ᆽ●₌)", "n",
-    "(=✪ᆽ✪=)", "n",
-    "ᨐ", "n",
-    "✧-✧", "n"
+'use strict';
+var hexDigits = '0123456789abcdef'.split('');
+function parse(ch) {
+    var code = ch.codePointAt(0);
+    return toHexDigit((code >> 12) & 15) +
+        toHexDigit((code >> 8) & 15) +
+        toHexDigit((code >> 4) & 15) +
+        toHexDigit(code & 15);
+}
+function toHexDigit(nibble) {
+    return hexDigits[nibble & 15];
+}
+function escapeUnicode(input, start, end) {
+    if (input == null) {
+        return input;
+    }
+    start = start != null && start >= 0 ? start : 0;
+    end = end != null && end >= 0 ? Math.min(end, input.length) : input.length;
+    var result = '';
+    for (var i = start; i < end; i++) {
+        result += "\\u".concat(parse(input[i]));
+    }
+    return result;
+}
+var eyes = ["● ●", "✧ ✧", "ㅇ ㅇ", "◕ ◕", "◓ ◓", "> <", ". .", "｡ ｡", "- -", "✪ ✪", "◔ ◔", "⦿ ⦿", "⦾ ⦾"];
+var mouths = ["ㅅ", "×", "_", "ꞈ", "∇", "｡", "ᆽ", "ꞈ", "︿"];
+var bodies = ["   ", "( )", "/ᐠ ᐟ\\"];
+var emote = ["  ", "❀", "ฅ", "*", "==", "✿"];
+var filler = [" ", "ᵐᵉᵒʷˎˊ˗", "🆄🆆🆄"];
+var hardcoded = [
+    "(●ㅅ● )",
+    "( ✧×✧)",
+    "(ㅇㅅㅇ❀)",
+    "(ฅ^･ㅅ･^)",
+    "(◕ㅅ◕ )",
+    "( ◓-◓)",
+    "(* >ㅅ<)",
+    "(   ∵   )",
+    "=^._.^= ∫",
+    "(=´∇｀=)",
+    "/ᐠ｡ꞈ｡ᐟ\\",
+    "✧/ᐠ-ꞈ-ᐟ\\",
+    "/ᐠ.ﮧ.ᐟ\\",
+    "/ᐠ. ｡.ᐟ\\ᵐᵉᵒʷˎˊ˗",
+    "/ᐠ-ᆽ-ᐟ\\",
+    "/ᐠ ̥  ̮  ̥ ᐟ\\ฅ",
+    "(₌◕ᆽ◕₌)",
+    "(₌●ᆽ●₌)",
+    "(=✪ᆽ✪=)",
+    "ᨐ",
+    "✧-✧"
 ];
 //pull hardcoded minty's for now until mintyconstructor is finished
-let random = Math.floor(Math.random() * (hardcoded.length / 2));
-let titleElement = document.getElementById('title');
-const dna = hardcoded[0 + (random * 2)];
+var random = Math.floor(Math.random() * (hardcoded.length / 2));
+var titleElement = document.getElementById('title');
+var dna = hardcoded[0 + (random * 2)];
 if (titleElement)
-    document.title = (0, escape_unicode_1.default)(dna);
-//minty-generator is supposed to pull strings to make a cat
-//and set the page title to a newly generated cat periodically
-//yes, i do want cute cats to adorn your browser bar
-//# sourceMappingURL=minty-generator.js.map
+    document.title = escapeUnicode(dna, 0, dna.length);
--- ts/minty-generator.ts
+++ ts/minty-generator.ts
@@ -1,37 +1,60 @@
-import escapeUnicode from '../js/escape-unicode';
+'use strict';
+const hexDigits = '0123456789abcdef'.split('');
+function parse(ch) {
+  	const code = ch.codePointAt(0);
+  	return toHexDigit((code >> 12) & 15) +
+    	toHexDigit((code >> 8) & 15) +
+    	toHexDigit((code >> 4) & 15) +
+    	toHexDigit(code & 15);
+}
+function toHexDigit(nibble) {
+  return hexDigits[nibble & 15];
+}
+function escapeUnicode(input, start, end) {
+	if (input == null) {
+    	return input;
+  	}
+  	start = start != null && start >= 0 ? start : 0;
+  	end = end != null && end >= 0 ? Math.min(end, input.length) : input.length;
+  	let result = '';
+  	for (let i = start; i < end; i++) {
+    	result += `\\u${parse(input[i])}`;
+  	}
+  	return result;
+}
 let eyes: string[] = ["● ●", "✧ ✧", "ㅇ ㅇ", "◕ ◕", "◓ ◓", "> <", ". .", "｡ ｡", "- -", "✪ ✪", "◔ ◔", "⦿ ⦿", "⦾ ⦾"];
 let mouths: string[] = ["ㅅ", "×", "_", "ꞈ", "∇", "｡", "ᆽ", "ꞈ", "︿"];
 let bodies: string[] = ["   ", "( )", "/ᐠ ᐟ\\"];
 let emote: string[] = ["  ", "❀", "ฅ", "*", "==", "✿"];
 let filler: string[] = [" ", "ᵐᵉᵒʷˎˊ˗", "🆄🆆🆄"];
 let hardcoded: string[] = [
-"(●ㅅ● )",          "n",
-"( ✧×✧)",          "n", 
-"(ㅇㅅㅇ❀)",        "n",
-"(ฅ^･ㅅ･^)",         "n",
-"(◕ㅅ◕ )",         "n",
-"( ◓-◓)",          "n",
-"(* >ㅅ<)",         "n",
-"(   ∵   )",        "n",
-"=^._.^= ∫",        "n",
-"(=´∇｀=)",         "n",
-"/ᐠ｡ꞈ｡ᐟ\\",          "n",
-"✧/ᐠ-ꞈ-ᐟ\\",        "n",
-"/ᐠ.ﮧ.ᐟ\\",          "n",
-"/ᐠ. ｡.ᐟ\\ᵐᵉᵒʷˎˊ˗",  "n",
-"/ᐠ-ᆽ-ᐟ\\",        "n",
-"/ᐠ ̥  ̮  ̥ ᐟ\\ฅ",      "n",
-"(₌◕ᆽ◕₌)",        "n",
-"(₌●ᆽ●₌)",         "n",
-"(=✪ᆽ✪=)",        "n",
-"ᨐ",               "n",
-"✧-✧",             "n"
+"(●ㅅ● )", 
+"( ✧×✧)", 
+"(ㅇㅅㅇ❀)",        
+"(ฅ^･ㅅ･^)",         
+"(◕ㅅ◕ )",         
+"( ◓-◓)",          
+"(* >ㅅ<)",         
+"(   ∵   )",        
+"=^._.^= ∫",        
+"(=´∇｀=)",         
+"/ᐠ｡ꞈ｡ᐟ\\",          
+"✧/ᐠ-ꞈ-ᐟ\\",        
+"/ᐠ.ﮧ.ᐟ\\",          
+"/ᐠ. ｡.ᐟ\\ᵐᵉᵒʷˎˊ˗",  
+"/ᐠ-ᆽ-ᐟ\\",        
+"/ᐠ ̥  ̮  ̥ ᐟ\\ฅ",      
+"(₌◕ᆽ◕₌)",        
+"(₌●ᆽ●₌)",         
+"(=✪ᆽ✪=)",        
+"ᨐ",               
+"✧-✧"
 ];
 //pull hardcoded minty's for now until mintyconstructor is finished
 let random = Math.floor(Math.random() * (hardcoded.length / 2));
 let titleElement = document.getElementById('title');
 const dna = hardcoded[0 + (random * 2)];
-if (titleElement) document.title = escapeUnicode(dna);
+if (titleElement) document.title = escapeUnicode(dna, 0, dna.length);
 //minty-generator is supposed to pull strings to make a cat
 //and set the page title to a newly generated cat periodically
 //yes, i do want cute cats to adorn your browser bar
