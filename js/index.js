3c0b4d683ba2d5e87eaa28519c2c978322e25798
MININT2 <minint.dsk@gmail.com>
2022-01-24 16:13:17 -0600

    update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 function main() {
     const canvas = document.querySelector("#glCanvas");
-    // Initialize the GL context
+    //initialize the gl context
     const gl = canvas.getContext("webgl");
-    // Only continue if WebGL is available and working
+    //only continue if webgl is available and working
     if (gl === null) {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
@@ -11,23 +11,23 @@ function main() {
     }
     resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    // Set clear color to black, fully opaque
+    //set clear color to #3b6dcb, fully opaque
     gl.clearColor(0.231, 0.427, 0.796, 1.0);
-    // Clear the color buffer with specified clear color
+    //clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
 function resizeCanvasToDisplaySize(canvas) {
-    // Lookup the size the browser is displaying the canvas in CSS pixels.
+    //lookup the size the browser is displaying the canvas in css pixels.
     const displayWidth  = canvas.clientWidth;
     const displayHeight = canvas.clientHeight;
     console.log("Viewport Size = " + displayHeight + "H" + displayWidth + "W");
-    // Check if the canvas is not the same size.
+    //check if the canvas is not the same size.
     const needResize = canvas.width  !== displayWidth ||
                        canvas.height !== displayHeight;   
     if (needResize) {
         console.log("Canvas Size = " + canvas.height + "H" + canvas.width + "W");
         console.log("Resizing now.");
-        // Make the canvas the same size
+        //make the canvas the same size
         canvas.width  = displayWidth;
         canvas.height = displayHeight;
     }
